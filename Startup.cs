b457498dffa0f7748f47047600609876cdaa39f2
Wallace <aw676914@ohio.edu>
2020-01-22 10:16:47 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Avery_MIS4200.Startup))]
+namespace Avery_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
