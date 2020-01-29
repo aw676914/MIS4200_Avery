5c8976491166bd96fef4a925e0526d3e8ca24c3d
Avery Wallace <aw676914@ohio.edu>
2020-01-29 09:33:00 -0500

    SD4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Avery_MIS4200.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My MIS4200 Test Description.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Info About Me.";
 
             return View();
         }
---
+++ Models/customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avery_MIS4200.Models
+{
+    public class customer
+    {
+        public string customerID {get; set;}
+        public string CustomerLastName { get; set; }
+        public string customerFirstName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime CustomerSince { get; set; }
+    }
+}
