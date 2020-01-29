81b7aa596e9ae7c01177d4acd55403cf6a144d9d
Avery Wallace <aw676914@ohio.edu>
2020-01-29 10:07:25 -0500

    Class

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -11,5 +11,12 @@ namespace Avery_MIS4200.Models
         [Key]
         public int orderNum { get; set; }
 
+        public string description { get; set; }
+
+        public DateTime orderdate { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public virtual customer customer { get; set; }
     }
 }
